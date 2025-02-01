3d6d0add3be468963e8d0fdf6f22c55984119bd5
MuhammadHussnain28 <muhammadhussnainrauf@gmail.com>
2025-02-01 12:12:10 +0500

    remote of disk:f vscode folder

---
+++ JS ASSIGN... 1 marksheet and calculator/app.js
@@ -0,0 +1,91 @@
+// mathMarks = +prompt("Enter your math marks out of 100 ?") ;
+// EnglishMarks = +prompt("Enter your English marks out of 100 ?") ;
+// CSMarks = +prompt("Enter your CS marks out of 100 ?") ;
+// urduMarks = +prompt("Enter your urdu marks out of 100 ?") ;
+// PstMarks = +prompt("Enter your Pst marks out of 100 ?") ;
+
+// totalMarks = mathMarks + EnglishMarks + CSMarks + urduMarks + PstMarks ;
+
+// document.write("<b><u><i>" + "YOUR MARKSHHET IS HERE" + "</b></u></i>" + "<br><br>")
+
+// document.write("Score of Maths:" , mathMarks + "<br><br>");
+// document.write("Score of EnglishMarks:" , EnglishMarks + "<br><br>");
+// document.write("Score of CSMarks:" , CSMarks + "<br><br>");
+// document.write("Score of urduMarks:" , urduMarks + "<br><br>");
+// document.write("Score of PstMarks:" , PstMarks + "<br><br>" );
+
+
+// document.write("Overall Score" , totalMarks  + "<br><br>")
+
+// if(totalMarks >= 450){
+//     document.write("Your garde is A*👌") ;
+// }
+// else if(totalMarks >= 400){
+//     document.write("Your garde is A👏") ;
+// }
+// else if(totalMarks >= 300){
+//     document.write("Your garde is B👍") ;
+// }
+// else{
+//     document.write("Your garde is F🤦‍♀️") ;
+// }
+
+
+// percentage = (totalMarks / 500 ) * 100 ;
+// document.write("<br><br>" + "Your percentage is: " , percentage , "%")
+
+// for(var table = 1 ; table <= 10 ; table++){
+//     console.log("Table of" , table)
+//     console.log(" ")
+//     for(var i = 1 ; i <= 10 ; i++){
+//         console.log(table + " x " + i + " = " + table*i)
+//     }
+//     console.log(" ")
+// }
+
+var arr = ["hussnain" , "huzaifa" , "hamza" , "uzair" ,"mubashir"]
+// console.log(typeof(arr))
+// console.log(arr.length)
+// console.log(arr.toString())
+// console.log(arr.join(" relative "))
+// console.log(arr.pop())
+// console.log(arr)
+// arr[3] = "saim"
+// console.log(arr.push("moosa" , "ghani"))
+// console.log(arr.shift())
+// console.log(arr.unshift("ahmed" , "usman"))
+// console.log(delete arr[0])
+// console.log(arr.length)
+// console.log(arr.splice(0,3));
+// console.log(arr.splice(0,0,"muhammad","anas","konain"))
+// console.log(arr.slice(0,2))
+
+
+
+// var str = "With HTML you can create your own Website.";
+// console.log(str[34]);
+// find "Website" index num
+// for (var i = 0; i < str.length; i++) {
+//   console.log(str.slice(i, i + 7));
+//   if (str.slice(i, i + 7) == "Website") {
+//     console.log(i, "indexNumber");
+//     break;
+//   }
+// }
+
+
+
+// var str = "With HTML you can create your own Website.";
+// for (var i = 0; i < str.length; i++) {
+//   if (str.slice(i, i + 4) == "HTML") {
+//     console.log(i);
+//     var leftSide = str.slice(0, i);
+//     var rightSide = str.slice(i + 4);
+//     console.log(leftSide + "HYPER TEXT" + rightSide);
+//     break;
+//   }
+// }
+
+// var myFractional = parseFloat("1.9999");
+// console.log(typeof(myFractional))
+
